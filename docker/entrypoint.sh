cbb957243b4f0bfcde74f87eba636c38cc54bf5a
Andre Detsch <andre.detsch@foundries.io>
2025-08-27 16:50:46 -0300

    docker: Add test environment based on Debian
    Define a docker compose application based on the one used in
    aktualizr-lite end-to-end tests. It includes a Debian Trixie container
    with all necessary dependencies installed to build and run fioup tests.
    
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

---
+++ docker/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+if [ -z $DEV_USER ] || [ -z $DEV_GROUP ]; then
+    echo "DEV_USER and DEV_GROUP environment variables must be set."
+    exit 1
+fi
+
+# Create a group with the specified GID if it doesn't already exist
+if ! getent group $DEV_GROUP >/dev/null; then
+    groupadd -g $DEV_GROUP devgrp
+fi
+
+# Create a user with the specified UID and GID if it doesn't already exist
+if ! getent passwd $DEV_USER >/dev/null; then
+    useradd -u $DEV_USER -g $DEV_GROUP -m dev
+fi
+
+# Change ownership of the home directory to the appuser
+chown -R dev:devgrp /home/dev
+
+chown dev:devgrp /var/run/docker/docker.sock
+
+chown -R dev:devgrp /var/sota
+chown -R dev:devgrp /usr/lib/sota/conf.d
+chown -R dev:devgrp /etc/sota/conf.d
+chown -R dev:devgrp /var/lib/docker
+
+ln -sfn ${PWD}/bin/fioup /usr/local/bin/fioup
+
+# Run the command as the created user
+exec gosu $DEV_USER:$DEV_GROUP "$@"
