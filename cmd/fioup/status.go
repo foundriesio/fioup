8d8aa8ca46e7245ac98cb5c4f7475d982852cc1d
Mike Sul <mike.sul@foundries.io>
2025-09-18 13:06:25 +0200

    status: Add status cmd with json output
    - Add API function to provide the current status of apps and status
      of the last or currently ongoing update.
    - Refactor the "status" command so it uses the API functions.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- cmd/fioup/status.go
+++ cmd/fioup/status.go
@@ -4,24 +4,102 @@
 package main
 
 import (
-	"github.com/foundriesio/fioup/internal/update"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/foundriesio/composeapp/pkg/compose"
+	"github.com/foundriesio/composeapp/pkg/update"
+	"github.com/foundriesio/fioup/pkg/fioup"
 	"github.com/spf13/cobra"
 )
 
+type (
+	statusReport struct {
+		// Represents the current status of apps from the last successful update.
+		// If no successful update exists, falls back to the status of apps detected locally.
+		CurrentStatus *fioup.CurrentStatus `json:"current_status"`
+		// Status of a pending or last update operation, if any
+		UpdateStatus *fioup.UpdateStatus `json:"update_status,omitempty"`
+	}
+	statusOptions struct {
+		Format string
+	}
+)
+
 func init() {
-	opts := update.UpdateOptions{}
+	opts := statusOptions{}
+
 	cmd := &cobra.Command{
 		Use:   "status",
-		Short: "Show information about the running target and pending update status, if any",
-		Run: func(cmd *cobra.Command, args []string) {
-			doStatus(&opts)
-		},
-		Args: cobra.NoArgs,
+		Short: "Show information about the currently running target/apps and pending or last update status, if any",
+		Args:  cobra.NoArgs,
 	}
+	cmd.Flags().StringVar(&opts.Format, "format", "text", "Format the output. Values: [text | json]")
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		switch opts.Format {
+		case "text", "json":
+			doStatus(cmd, &opts)
+		default:
+			return fmt.Errorf("invalid value for --format: %s (must be text or json)", opts.Format)
+		}
+		return nil
+	}
+
 	rootCmd.AddCommand(cmd)
 }
 
-func doStatus(opts *update.UpdateOptions) {
-	err := update.Status(config, opts)
-	DieNotNil(err, "Failed to get status infomation")
+func doStatus(cmd *cobra.Command, opts *statusOptions) {
+	cs, err := fioup.GetCurrentStatus(cmd.Context(), composeConfig)
+	DieNotNil(err, "failed to get current status")
+	us, err := fioup.GetUpdateStatus(composeConfig)
+	DieNotNil(err, "failed to get update status")
+
+	if opts.Format == "json" {
+		if b, err := json.Marshal(statusReport{CurrentStatus: cs, UpdateStatus: us}); err != nil {
+			DieNotNil(err, "failed to marshal status report")
+		} else {
+			fmt.Println(string(b))
+		}
+		return
+	}
+	fmt.Println("Current status:")
+	fmt.Printf("  Target ID:\t%s\n", cs.TargetID)
+	fmt.Printf("  Update ID:\t%s\n", cs.UpdateID)
+	fmt.Printf("  Completed at:\t%s\n", cs.CompletedAt.Local().Format(time.DateTime))
+	fmt.Println("  Apps:")
+	for _, app := range cs.AppStatuses {
+		fmt.Printf("\t\t[%s]: \n", app.Name)
+		fmt.Printf("\t\t  %s\n", app.URI)
+		fmt.Printf("\t\t  fetched:%v; installed:%v; running:%v\n", app.Fetched, app.Installed, app.Running)
+		fmt.Println()
+	}
+	ongoing := true
+	if us.State == update.StateCompleted || us.State == update.StateCanceled || us.State == update.StateFailed {
+		fmt.Println("Last update status:")
+		ongoing = false
+	} else {
+		fmt.Println("Ongoing update status:")
+	}
+	fmt.Printf("  Target ID:\t%s\n", us.TargetID)
+	fmt.Printf("  Update ID:\t%s\n", us.ID)
+	fmt.Printf("  State:\t%s\n", us.State)
+	fmt.Printf("  Started at:\t%s\n", us.StartTime.Local().Format(time.DateTime))
+	if ongoing {
+		fmt.Printf("  Updated at:\t%s\n", us.StartTime.Local().Format(time.DateTime))
+	} else {
+		fmt.Printf("  Completed at:\t%s\n", us.StartTime.Local().Format(time.DateTime))
+	}
+
+	fmt.Println("  Apps:")
+	for _, app := range us.Apps {
+		fmt.Printf("\t\t- %s\n", app)
+	}
+	fmt.Printf("  Update size:\t%s, %d blobs\n", compose.FormatBytesInt64(us.Size.Bytes), us.Size.NumBlobs)
+	if us.FetchedSize.Bytes < us.Size.Bytes {
+		fmt.Printf("  Fetched:\t%s, %d blobs\n", compose.FormatBytesInt64(us.FetchedSize.Bytes), us.FetchedSize.NumBlobs)
+	}
+	if ongoing {
+		fmt.Printf("  Progress:\t%d\n", us.Progress)
+	}
 }
---
+++ pkg/fioup/status.go
@@ -0,0 +1,109 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+package fioup
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/foundriesio/composeapp/pkg/compose"
+	"github.com/foundriesio/composeapp/pkg/update"
+)
+
+type (
+	AppStatus struct {
+		URI       string `json:"uri"`
+		Name      string `json:"name"`
+		Fetched   bool   `json:"fetched"`
+		Installed bool   `json:"installed"`
+		Running   bool   `json:"running"`
+	}
+
+	CurrentStatus struct {
+		UpdateID    string      `json:"update_id"`
+		TargetID    string      `json:"target_id"`
+		AppStatuses []AppStatus `json:"apps"`
+		CompletedAt time.Time   `json:"completed_at"`
+	}
+
+	BlobStats struct {
+		Bytes    int64 `json:"bytes"`
+		NumBlobs int   `json:"num_blobs"`
+	}
+
+	UpdateStatus struct {
+		ID          string       `json:"id"`
+		TargetID    string       `json:"target_id"`
+		State       update.State `json:"state"`
+		StartTime   time.Time    `json:"start_time"`
+		UpdatedAt   time.Time    `json:"updated_at"`
+		Apps        []string     `json:"apps"`
+		Size        BlobStats    `json:"size"`
+		FetchedSize BlobStats    `json:"fetched_size"`
+		Progress    int          `json:"progress"`
+	}
+)
+
+func GetCurrentStatus(ctx context.Context, cfg *compose.Config) (*CurrentStatus, error) {
+	currentStatus := CurrentStatus{
+		AppStatuses: []AppStatus{},
+	}
+	var appURIs []string
+
+	if lastUpdate, err := update.GetLastSuccessfulUpdate(cfg); err == nil {
+		appURIs = lastUpdate.URIs
+		currentStatus.UpdateID = lastUpdate.ID
+		currentStatus.TargetID = lastUpdate.ClientRef
+		currentStatus.CompletedAt = lastUpdate.UpdateTime
+	} else {
+		// No successful last update exists, falls back to the status of apps detected locally;
+		// if no apps are specified to `CheckAppsStatus` then it will check all apps found locally
+		currentStatus.TargetID = "unknown"
+	}
+	s, err := compose.CheckAppsStatus(ctx, cfg, appURIs, compose.WithQuickCheckFetch(true))
+	if err != nil {
+		return nil, fmt.Errorf("failed to check apps' status: %w", err)
+	}
+
+	areFetched := s.AreFetched()
+	areInstalled := s.AreInstalled()
+	areRunning := s.AreRunning()
+
+	for _, app := range s.Apps {
+		currentStatus.AppStatuses = append(currentStatus.AppStatuses, AppStatus{
+			URI:  app.Ref().String(),
+			Name: app.Name(),
+			// TODO: per-app status check instead of global, add wrapper functions over `compose.AppsStatus` for that
+			Fetched:   areFetched,
+			Installed: areInstalled,
+			Running:   areRunning,
+		})
+	}
+	return &currentStatus, nil
+}
+
+func GetUpdateStatus(cfg *compose.Config) (*UpdateStatus, error) {
+	s, err := update.GetLastUpdate(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get last update: %w", err)
+	}
+	return &UpdateStatus{
+		ID:        s.ID,
+		TargetID:  s.ClientRef,
+		State:     s.State,
+		StartTime: s.CreationTime,
+		UpdatedAt: s.UpdateTime,
+		Apps:      s.URIs,
+		Size: BlobStats{
+			Bytes:    s.TotalBlobsBytes,
+			NumBlobs: len(s.Blobs),
+		},
+		FetchedSize: BlobStats{
+			Bytes:    s.FetchedBytes,
+			NumBlobs: s.FetchedBlobs,
+		},
+		Progress: s.Progress,
+	}, nil
+}
