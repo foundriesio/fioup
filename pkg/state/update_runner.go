99b9273a3ef6cf569f94592362ce3bd2ca240386
Andre Detsch <andre.detsch@foundries.io>
2025-09-29 14:59:45 -0300

    events: Send events asynchronously
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

--- pkg/state/update_runner.go
+++ pkg/state/update_runner.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/foundriesio/fioup/internal/events"
 	internal "github.com/foundriesio/fioup/internal/update"
-	"github.com/foundriesio/fioup/pkg/client"
 	"github.com/foundriesio/fioup/pkg/config"
 	"github.com/foundriesio/fioup/pkg/target"
 )
@@ -38,21 +37,21 @@ func (sm *UpdateRunner) Run(ctx context.Context, cfg *config.Config) error {
 	if err := internal.InitializeDatabase(cfg.GetDBPath()); err != nil {
 		return err
 	}
-	gwClient, err := client.NewGatewayClient(cfg, nil, "")
+	sm.ctx.Config = cfg
+
+	eventSender, err := events.NewEventSender(cfg)
 	if err != nil {
 		return err
 	}
-	sm.ctx.Config = cfg
-	sm.ctx.EventSender = &EventSender{
-		dbPath:   cfg.GetDBPath(),
-		gwClient: gwClient,
-	}
+	eventSender.Start()
+	defer eventSender.Stop()
+	sm.ctx.EventSender = eventSender
+
 	stateCounter := 1
 	for _, s := range sm.states {
 		sm.ctx.CurrentState = s.Name()
 		fmt.Printf("[%d/5] %s:", stateCounter, s.Name())
 		err := s.Execute(ctx, sm.ctx)
-		sm.ctx.EventSender.FlushEvents()
 		if err != nil {
 			return fmt.Errorf("failed at state %s: %w", s.Name(), err)
 		}
@@ -62,26 +61,7 @@ func (sm *UpdateRunner) Run(ctx context.Context, cfg *config.Config) error {
 }
 
 func (u *UpdateContext) SendEvent(event events.EventTypeValue, success ...bool) {
-	if err := u.EventSender.SendEvent(event, u.UpdateRunner.Status().ID, u.ToTarget, success...); err != nil {
+	if err := u.EventSender.EnqueueEvent(event, u.UpdateRunner.Status().ID, u.ToTarget, success...); err != nil {
 		slog.Error("failed to send event", "event", event, "err", err)
-	}
-}
-
-func (s *EventSender) SendEvent(eventType events.EventTypeValue, updateID string, toTarget target.Target, success ...bool) error {
-	var completionStatus *bool
-	if len(success) > 0 {
-		completionStatus = &success[0]
-	}
-	if eventType == events.InstallationCompleted && completionStatus != nil && *completionStatus {
-		// Update list of apps and target ID if update is successful
-		s.gwClient.UpdateHeaders(toTarget.AppNames(), toTarget.ID)
-	}
-	evt := events.NewEvent(eventType, "", completionStatus, updateID, toTarget.ID, toTarget.Version)
-	return events.SaveEvent(s.dbPath, &evt[0])
-}
-
-func (s *EventSender) FlushEvents() {
-	if err := events.FlushEvents(s.dbPath, s.gwClient); err != nil {
-		slog.Error("failed to flush events", "err", err)
 	}
 }
