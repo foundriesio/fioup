191115f7446a9478c1d7fe3cc10fdbd6d12890e7
Andre Detsch <andre.detsch@foundries.io>
2025-10-01 16:31:49 -0300

    daemon: Move daemon implementation to API based code
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

--- cmd/fioup/daemon.go
+++ cmd/fioup/daemon.go
@@ -4,27 +4,47 @@
 package main
 
 import (
+	"errors"
 	"log/slog"
+	"strconv"
+	"time"
 
-	"github.com/foundriesio/fioup/internal/update"
+	"github.com/foundriesio/fioup/pkg/api"
+	"github.com/foundriesio/fioup/pkg/state"
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	opts := update.UpdateOptions{}
 	cmd := &cobra.Command{
 		Use:   "daemon",
 		Short: "Start the update agent daemon",
 		Run: func(cmd *cobra.Command, args []string) {
-			doDaemon(cmd, &opts)
+			doDaemon(cmd)
 		},
 		Args: cobra.NoArgs,
 	}
-	addCommonOptions(cmd, &opts)
 	rootCmd.AddCommand(cmd)
 }
 
-func doDaemon(cmd *cobra.Command, opts *update.UpdateOptions) {
-	update.Daemon(cmd.Context(), config, opts)
-	slog.Info("Daemon exited")
+func doDaemon(cmd *cobra.Command) {
+	pollingSecStr := config.TomlConfig().GetDefault("uptane.polling_seconds", "300")
+	pollingSec, err := strconv.Atoi(pollingSecStr)
+	if err != nil || pollingSec <= 0 {
+		pollingSec = 300
+		slog.Debug("Invalid value for uptane.polling_seconds. Using default value", "value", pollingSecStr, "default", pollingSec)
+	}
+	interval := time.Duration(time.Duration(pollingSec) * time.Second)
+	ctx := cmd.Context()
+	for {
+		err := api.Update(cmd.Context(), config, -1, true)
+		if err != nil && !errors.Is(err, state.ErrCheckNoUpdate) {
+			slog.Error("Error during update", "error", err)
+		}
+		slog.Info("Waiting before next check...", "interval", interval)
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(interval):
+		}
+	}
 }
