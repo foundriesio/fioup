9cbf319c4970ce81eedeb450620341cde8ba712d
Andy Doan <andy@foundries.io>
2025-10-02 13:40:32 -0500

    client: Add a "self" API to support `whoami`
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ pkg/client/self.go
@@ -0,0 +1,29 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+package client
+
+import (
+	"time"
+)
+
+type DgTimeStamp float32
+
+func (ts DgTimeStamp) AsTime() time.Time {
+	return time.Unix(int64(ts), 0)
+}
+
+type Device struct {
+	RepoId    string      `json:"repo_id"`
+	Uuid      string      `json:"uuid"`
+	Name      string      `json:"name"`
+	CreatedAt DgTimeStamp `json:"created_at"`
+	LastSeen  DgTimeStamp `json:"last_seen"`
+	PubKey    string      `json:"pubkey"`
+	Tag       string      `json:"tag"`
+}
+
+func (c GatewayClient) Self() (*Device, error) {
+	var d Device
+	return &d, c.getJson("/device", &d)
+}
