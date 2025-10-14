b27dda6ab9044c09f5cef97049885f4801e7c582
Mike Sul <mike.sul@foundries.io>
2025-10-14 18:13:38 +0200

    api: Remove auto-rollback if update failure
    Let's not do the auto-rollback within the CLI commands (update and start).
    Instead let a user decide what to do next since in some cases it makes
    sense to retry in some cases not.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- pkg/api/start.go
+++ pkg/api/start.go
@@ -5,16 +5,14 @@ package api
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/foundriesio/composeapp/pkg/update"
 	"github.com/foundriesio/fioup/pkg/config"
 	"github.com/foundriesio/fioup/pkg/state"
-	"github.com/pkg/errors"
 )
 
 func Start(ctx context.Context, cfg *config.Config) error {
-	updateRunner := state.NewUpdateRunner([]state.ActionState{
+	return state.NewUpdateRunner([]state.ActionState{
 		&state.Check{
 			Action:         "start",
 			UpdateTargets:  false,
@@ -26,24 +24,6 @@ func Start(ctx context.Context, cfg *config.Config) error {
 			ToVersion: -1,
 		},
 		&state.Init{},
-		&state.Fetch{},
-		&state.Install{},
-		&state.Start{},
-	})
-	err := updateRunner.Run(ctx, cfg)
-	if !errors.Is(err, state.ErrStartFailed) {
-		return err
-	}
-	// if app failed to start, do rollback
-	fmt.Printf("Update failed, rolling back to previous version; err: %s\n", err.Error())
-	return state.NewUpdateRunner([]state.ActionState{
-		&state.Check{
-			Action:         "rollback",
-			UpdateTargets:  false,
-			AllowNewUpdate: true,
-			ToVersion:      updateRunner.GetFromTarget().Version,
-		},
-		&state.Init{CheckState: true},
 		&state.Fetch{},
 		&state.Install{},
 		&state.Start{},
--- pkg/api/update.go
+++ pkg/api/update.go
@@ -5,15 +5,13 @@ package api
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/foundriesio/fioup/pkg/config"
 	"github.com/foundriesio/fioup/pkg/state"
-	"github.com/pkg/errors"
 )
 
 func Update(ctx context.Context, cfg *config.Config, toVersion int, skipIfRunning bool) error {
-	updateRunner := state.NewUpdateRunner([]state.ActionState{
+	return state.NewUpdateRunner([]state.ActionState{
 		&state.Check{
 			Action:         "update",
 			UpdateTargets:  true,
@@ -22,24 +20,6 @@ func Update(ctx context.Context, cfg *config.Config, toVersion int, skipIfRunnin
 			ToVersion:      toVersion,
 		},
 		&state.Init{},
-		&state.Fetch{},
-		&state.Install{},
-		&state.Start{},
-	})
-	err := updateRunner.Run(ctx, cfg)
-	if !errors.Is(err, state.ErrStartFailed) {
-		return err
-	}
-	// if app failed to start, do rollback
-	fmt.Printf("Update failed, rolling back to previous version; err: %s\n", err.Error())
-	return state.NewUpdateRunner([]state.ActionState{
-		&state.Check{
-			Action:         "rollback",
-			UpdateTargets:  false,
-			AllowNewUpdate: true,
-			ToVersion:      updateRunner.GetFromTarget().Version,
-		},
-		&state.Init{CheckState: true},
 		&state.Fetch{},
 		&state.Install{},
 		&state.Start{},
