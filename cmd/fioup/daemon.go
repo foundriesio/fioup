bf821cfccb46ad8984a7f4cd2cc8a54218d0eb31
Andre Detsch <andre.detsch@foundries.io>
2025-10-15 19:13:02 -0300

    daemon: Add --run-once option (hidden)
    When specified, the daemon will perform a single update check and then
    exit. This is useful for testing the daemon functionality automacially.
    
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

--- cmd/fioup/daemon.go
+++ cmd/fioup/daemon.go
@@ -14,19 +14,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type (
+	daemonOptions struct {
+		runOnce bool
+	}
+)
+
 func init() {
+	opts := daemonOptions{
+		runOnce: false,
+	}
 	cmd := &cobra.Command{
 		Use:   "daemon",
 		Short: "Start the update agent daemon",
 		Run: func(cmd *cobra.Command, args []string) {
-			doDaemon(cmd)
+			doDaemon(cmd, &opts)
 		},
 		Args: cobra.NoArgs,
 	}
+	cmd.Flags().BoolVar(&opts.runOnce, "run-once", false, "Run a single update check and exit.")
+	_ = cmd.Flags().MarkHidden("run-once")
 	rootCmd.AddCommand(cmd)
 }
 
-func doDaemon(cmd *cobra.Command) {
+func doDaemon(cmd *cobra.Command, opts *daemonOptions) {
 	pollingSecStr := config.TomlConfig().GetDefault("uptane.polling_seconds", "300")
 	pollingSec, err := strconv.Atoi(pollingSecStr)
 	if err != nil || pollingSec <= 0 {
@@ -39,6 +50,10 @@ func doDaemon(cmd *cobra.Command) {
 		err := api.Update(cmd.Context(), config, -1, api.WithMaxAttempts(3))
 		if err != nil && !errors.Is(err, state.ErrCheckNoUpdate) {
 			slog.Error("Error during update", "error", err)
+		}
+		if opts.runOnce {
+			slog.Debug("Run once mode, exiting")
+			return
 		}
 		slog.Info("Waiting before next check...", "interval", interval)
 		select {
