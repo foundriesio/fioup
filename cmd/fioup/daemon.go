ff6b2b8c51a64df8bdcca772ba1d85ca840582cc
Andy Doan <andy@foundries.io>
2025-10-20 14:20:34 -0500

    daemon: Add fioconfig support to main loop
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cmd/fioup/daemon.go
+++ cmd/fioup/daemon.go
@@ -6,9 +6,11 @@ package main
 import (
 	"errors"
 	"log/slog"
+	"os"
 	"strconv"
 	"time"
 
+	fioconfig "github.com/foundriesio/fioconfig/app"
 	"github.com/foundriesio/fioup/internal/events"
 	"github.com/foundriesio/fioup/pkg/api"
 	"github.com/foundriesio/fioup/pkg/client"
@@ -17,10 +19,25 @@ import (
 )
 
 type (
+	fioconfigOpts struct {
+		secretsDir      string
+		unsafeHandlers  bool
+		configExtracted bool
+	}
+
 	daemonOptions struct {
 		runOnce bool
+
+		configEnabled bool
+		fioconfig     fioconfigOpts
 	}
 )
+
+func (opts *fioconfigOpts) ApplyToCmd(cmd *cobra.Command) {
+	cmd.Flags().StringVar(&opts.secretsDir, "secrets-dir", "/run/secrets", "Directory to hold FioConfig secrets when enabled.")
+	cmd.Flags().BoolVar(&opts.unsafeHandlers, "unsafe-handlers", false, "Enable unsafe FioConfig handlers.")
+	_ = cmd.Flags().MarkHidden("unsafe-handlers")
+}
 
 func init() {
 	opts := daemonOptions{
@@ -35,7 +52,9 @@ func init() {
 		Args: cobra.NoArgs,
 	}
 	cmd.Flags().BoolVar(&opts.runOnce, "run-once", false, "Run a single update check and exit.")
+	cmd.Flags().BoolVar(&opts.configEnabled, "fioconfig", true, "Include FioConfig daemon logic.")
 	_ = cmd.Flags().MarkHidden("run-once")
+	opts.fioconfig.ApplyToCmd(cmd)
 	rootCmd.AddCommand(cmd)
 }
 
@@ -54,6 +73,17 @@ func doDaemon(cmd *cobra.Command, opts *daemonOptions) {
 		slog.Error("Failed to create gateway client", "error", err)
 		return
 	}
+
+	var configApp *fioconfig.App
+
+	if opts.configEnabled {
+		configApp, err = fioconfig.NewAppWithConfig(config.TomlConfig(), opts.fioconfig.secretsDir, opts.fioconfig.unsafeHandlers)
+		if err != nil {
+			slog.Error("Failed to create FioConfig handle", "error", err)
+			return
+		}
+	}
+
 	if eventSender, err = events.NewEventSender(config, gwClient); err != nil {
 		slog.Error("Failed to create event sender", "error", err)
 		return
@@ -62,6 +92,10 @@ func doDaemon(cmd *cobra.Command, opts *daemonOptions) {
 	defer eventSender.Stop()
 
 	for {
+		if opts.configEnabled {
+			_ = configCheck(&opts.fioconfig, configApp)
+		}
+
 		err := api.Update(cmd.Context(), config, -1,
 			api.WithGatewayClient(gwClient),
 			api.WithEventSender(eventSender),
@@ -80,4 +114,31 @@ func doDaemon(cmd *cobra.Command, opts *daemonOptions) {
 		case <-time.After(interval):
 		}
 	}
+}
+
+func configCheck(config *fioconfigOpts, app *fioconfig.App) error {
+	if _, err := os.Stat(config.secretsDir); os.IsNotExist(err) {
+		slog.Debug("Creating FioConfig secrets directory", "dir", config.secretsDir)
+		if err := os.MkdirAll(config.secretsDir, 0o700); err != nil {
+			slog.Error("Failed to create secrets directory", "dir", config.secretsDir, "error", err)
+			return err
+		}
+	}
+	if !config.configExtracted {
+		slog.Debug("Running FioConfig secret extraction")
+		if err := app.Extract(); err != nil {
+			slog.Error("FioConfig secret extraction failed", "error", err)
+			return err
+		} else {
+			slog.Debug("FioConfig extraction completed successfully")
+			config.configExtracted = true
+		}
+	}
+	if err := app.CheckIn(); err != nil {
+		if err != fioconfig.NotModifiedError {
+			slog.Error("FioConfig check-in failed", "error", err)
+			return err
+		}
+	}
+	return nil
 }
