7aa77a21579f0633e29f4eb2db21fd6997eeda0f
Andy Doan <andy@foundries.io>
2025-10-28 10:04:23 -0500

    daemon: Break out update logic into its own function
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cmd/fioup/daemon.go
+++ cmd/fioup/daemon.go
@@ -4,8 +4,10 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"log/slog"
+	"os"
 	"strconv"
 	"time"
 
@@ -76,39 +78,8 @@ func doDaemon(cmd *cobra.Command, opts *daemonOptions) {
 	}
 
 	for {
-		err := api.Update(cmd.Context(), config, -1,
-			api.WithGatewayClient(gwClient),
-			api.WithEventSender(eventSender),
-			api.WithRequireLatest(true),
-			api.WithMaxAttempts(3),
-			api.WithPreStateHandler(preStateHandler),
-			api.WithPostStateHandler(postStateHandler),
-			api.WithFetchProgressHandler(update.GetFetchProgressPrinter(update.WithIndentation(8))),
-			api.WithInstallProgressHandler(update.GetInstallProgressPrinter(update.WithIndentation(8))),
-			api.WithStartProgressHandler(appStartHandler))
-		if err != nil && errors.Is(err, state.ErrNewerVersionIsAvailable) {
-			slog.Info("Cancelling current update, going to start a new one for the newer version")
-			_, err := api.Cancel(cmd.Context(), config)
-			if err != nil {
-				slog.Error("Error canceling old update", "error", err)
-			} else {
-				// If cancelation was successful, proceed without waiting
-				continue
-			}
-		} else if err != nil && errors.Is(err, state.ErrStartFailed) {
-			slog.Info("Error starting updated target", "error", err)
-			if !opts.runOnce {
-				// Retry installation, or do a sync update, without waiting
-				// If runOnce is set, exit execution in the return statement bellow
-				continue
-			}
-		} else if err != nil && !errors.Is(err, state.ErrCheckNoUpdate) {
-			slog.Error("Error during update", "error", err)
-		}
-		if opts.runOnce {
-			slog.Debug("Run once mode, exiting")
-			DieNotNil(err)
-			return
+		if nowait := updateCheck(cmd.Context(), opts, gwClient, eventSender); nowait {
+			continue
 		}
 		slog.Info("Waiting before next check...", "interval", interval)
 		select {
@@ -117,4 +88,42 @@ func doDaemon(cmd *cobra.Command, opts *daemonOptions) {
 		case <-time.After(interval):
 		}
 	}
+}
+
+func updateCheck(ctx context.Context, opts *daemonOptions, gwClient *client.GatewayClient, eventSender *events.EventSender) (nowait bool) {
+	err := api.Update(ctx, config, -1,
+		api.WithGatewayClient(gwClient),
+		api.WithEventSender(eventSender),
+		api.WithRequireLatest(true),
+		api.WithMaxAttempts(3),
+		api.WithPreStateHandler(preStateHandler),
+		api.WithPostStateHandler(postStateHandler),
+		api.WithFetchProgressHandler(update.GetFetchProgressPrinter(update.WithIndentation(8))),
+		api.WithInstallProgressHandler(update.GetInstallProgressPrinter(update.WithIndentation(8))),
+		api.WithStartProgressHandler(appStartHandler))
+	if err != nil && errors.Is(err, state.ErrNewerVersionIsAvailable) {
+		slog.Info("Cancelling current update, going to start a new one for the newer version")
+		_, err := api.Cancel(ctx, config)
+		if err != nil {
+			slog.Error("Error canceling old update", "error", err)
+		} else {
+			// If cancelation was successful, proceed without waiting
+			nowait = true
+		}
+	} else if err != nil && errors.Is(err, state.ErrStartFailed) {
+		slog.Info("Error starting updated target", "error", err)
+		if !opts.runOnce {
+			// Retry installation, or do a sync update, without waiting
+			// If runOnce is set, exit execution in the return statement bellow
+			nowait = true
+		}
+	} else if err != nil && !errors.Is(err, state.ErrCheckNoUpdate) {
+		slog.Error("Error during update", "error", err)
+	}
+	if opts.runOnce {
+		slog.Debug("Run once mode, exiting")
+		DieNotNil(err)
+		os.Exit(0)
+	}
+	return
 }
