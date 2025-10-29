84825e93ecac72862a22188803ba03949544f66f
Andy Doan <andy@foundries.io>
2025-10-29 12:54:02 -0500

    daemon: Change run-once logic
    Run-once is a very targetted function for e2e testing. This change makes
    its behavior more explicit and as close to our actual normal daemon
    logic as possible:
     * using same event sender
     * using checkUpdates function with no special conditionals
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cmd/fioup/daemon.go
+++ cmd/fioup/daemon.go
@@ -23,12 +23,7 @@ import (
 )
 
 type (
-	daemonOptions struct {
-		runOnce bool
-	}
-
 	updater struct {
-		opts   *daemonOptions
 		gw     *client.GatewayClient
 		sender *events.EventSender
 
@@ -37,26 +32,19 @@ type (
 )
 
 func init() {
-	opts := daemonOptions{
-		runOnce: false,
-	}
 	cmd := &cobra.Command{
 		Use:   "daemon",
 		Short: "Start the update agent daemon",
 		Run: func(cmd *cobra.Command, args []string) {
-			doDaemon(cmd, &opts)
+			doDaemon(cmd)
 		},
 		Args: cobra.NoArgs,
 	}
-	cmd.Flags().BoolVar(&opts.runOnce, "run-once", false, "Run a single update check and exit.")
-	_ = cmd.Flags().MarkHidden("run-once")
 	rootCmd.AddCommand(cmd)
 }
 
-func NewUpdater(opts *daemonOptions) *updater {
-	u := updater{
-		opts: opts,
-	}
+func NewUpdater() *updater {
+	u := updater{}
 	u.reload(false)
 	return &u
 }
@@ -74,11 +62,9 @@ func (u *updater) reload(reloadConfig bool) {
 	u.gw, err = client.NewGatewayClient(config, nil, "")
 	DieNotNil(err, "Failed to create gateway client")
 
-	if !u.opts.runOnce {
-		u.sender, err = events.NewEventSender(config, u.gw)
-		DieNotNil(err, "Failed to create event sender")
-		u.sender.Start()
-	}
+	u.sender, err = events.NewEventSender(config, u.gw)
+	DieNotNil(err, "Failed to create event sender")
+	u.sender.Start()
 
 	pollingSecStr := config.TomlConfig().GetDefault("uptane.polling_seconds", "300")
 	pollingSec, err := strconv.Atoi(pollingSecStr)
@@ -90,7 +76,7 @@ func (u *updater) reload(reloadConfig bool) {
 	u.sleepInterval = time.Duration(time.Duration(pollingSec) * time.Second)
 }
 
-func doDaemon(cmd *cobra.Command, opts *daemonOptions) {
+func doDaemon(cmd *cobra.Command) {
 	slog.Info("Daemon starting", "pid", os.Getpid())
 	ctx, cancel := signal.NotifyContext(cmd.Context(), os.Interrupt)
 	defer cancel()
@@ -98,17 +84,30 @@ func doDaemon(cmd *cobra.Command, opts *daemonOptions) {
 	sigHUP := make(chan os.Signal, 1)
 	signal.Notify(sigHUP, syscall.SIGHUP)
 
-	updater := NewUpdater(opts)
+	updater := NewUpdater()
 	defer updater.Close()
 
 	for {
-		if nowait := updater.checkUpdates(ctx); nowait {
+		nowait, err := updater.checkUpdates(ctx)
+		updater.checkForCI(err)
+		if nowait {
 			continue
 		}
 
 		if reloadConfig := updater.sleep(ctx, sigHUP); reloadConfig {
 			updater.reload(true)
 		}
+	}
+}
+
+// checkForCI looks to see if we are doing e2e testing and will exit the
+// the daemon.
+func (u updater) checkForCI(err error) {
+	if os.Getenv("FIOUP_E2E_RUNONCE") == "1" {
+		slog.Info("e2e running, exiting daemon")
+		u.Close()
+		DieNotNil(err)
+		os.Exit(0)
 	}
 }
 
@@ -135,8 +134,8 @@ func (u updater) sleep(ctx context.Context, sigHUP chan os.Signal) (reloadConfig
 	return
 }
 
-func (u updater) checkUpdates(ctx context.Context) (nowait bool) {
-	err := api.Update(ctx, config, -1,
+func (u updater) checkUpdates(ctx context.Context) (nowait bool, err error) {
+	err = api.Update(ctx, config, -1,
 		api.WithGatewayClient(u.gw),
 		api.WithEventSender(u.sender),
 		api.WithRequireLatest(true),
@@ -157,18 +156,10 @@ func (u updater) checkUpdates(ctx context.Context) (nowait bool) {
 		}
 	} else if err != nil && errors.Is(err, state.ErrStartFailed) {
 		slog.Info("Error starting updated target", "error", err)
-		if !u.opts.runOnce {
-			// Retry installation, or do a sync update, without waiting
-			// If runOnce is set, exit execution in the return statement bellow
-			nowait = true
-		}
+		// Retry installation, or do a sync update, without waiting
+		nowait = true
 	} else if err != nil && !errors.Is(err, state.ErrCheckNoUpdate) {
 		slog.Error("Error during update", "error", err)
-	}
-	if u.opts.runOnce {
-		slog.Debug("Run once mode, exiting")
-		DieNotNil(err)
-		os.Exit(0)
 	}
 	return
 }
