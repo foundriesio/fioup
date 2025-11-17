a3b808c738aa3bf21e885445116b6d00a227e023
Andre Detsch <andre.detsch@foundries.io>
2025-11-17 10:05:13 -0300

    stop: Only generate InstallationStarted event after checking state
    This fixes event duplication when resuming an installation.
    
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

--- pkg/state/stop.go
+++ pkg/state/stop.go
@@ -16,8 +16,6 @@ type Stop struct{}
 
 func (s *Stop) Name() ActionName { return "Stopping" }
 func (s *Stop) Execute(ctx context.Context, updateCtx *UpdateContext) error {
-	// Installation starts from stopping the currently running apps that are being updated
-	updateCtx.SendEvent(events.InstallationStarted)
 	currentState := updateCtx.UpdateRunner.Status().State
 	if currentState.IsOneOf(update.StateInstalled, update.StateStarting, update.StateStarted) {
 		// No need to stop apps if the ongoing update is already in installing, installed, starting, or started state
@@ -27,6 +25,8 @@ func (s *Stop) Execute(ctx context.Context, updateCtx *UpdateContext) error {
 		return fmt.Errorf("invalid state %s for stopping apps; must be in state %s or %s", currentState,
 			update.StateFetched, update.StateInstalling)
 	}
+	// Installation starts from stopping the currently running apps that are being updated
+	updateCtx.SendEvent(events.InstallationStarted)
 	// Stop apps being updated before installing their updates
 	return compose.StopApps(ctx, updateCtx.Config.ComposeConfig(), updateCtx.FromTarget.AppURIs())
 }
