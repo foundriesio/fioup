8ec3cb7af2a61007b69cd750160828e0d91f7217
Mike Sul <mike.sul@foundries.io>
2025-11-19 12:21:41 +0100

    start: Retry completion of successfully started update
    Update completion (uninstalling and removing dangling blobs) may fail
    under unexpected condition after successful update start.
    It makes sense to retry the completion in such the case, and if it fails
    again then retry without dangling blobs pruning.
    If it still fails then do the force update completion, so the update is
    considered as a successful and a user can do new updates.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- pkg/state/start.go
+++ pkg/state/start.go
@@ -28,13 +28,40 @@ func (s *Start) Name() ActionName { return "Starting" }
 func (s *Start) Execute(ctx context.Context, updateCtx *UpdateContext) error {
 	err := updateCtx.UpdateRunner.Start(ctx, compose.WithStartProgressHandler(s.ProgressHandler))
 	if err == nil {
-		if err := updateCtx.UpdateRunner.Complete(ctx, update.CompleteWithPruning()); err != nil {
-			slog.Debug("failed to complete update with pruning", "error", err)
-		}
+		updateCtx.completeUpdate(ctx)
 		updateCtx.Client.UpdateHeaders(updateCtx.ToTarget.AppNames(), updateCtx.ToTarget.ID)
 	} else {
 		err = fmt.Errorf("%w: %s", ErrStartFailed, err.Error())
 	}
 	updateCtx.SendEvent(events.InstallationCompleted, err == nil)
 	return err
+}
+
+func (u *UpdateContext) completeUpdate(ctx context.Context) {
+	var err error
+	// 1. First attempt with pruning
+	if err = u.UpdateRunner.Complete(ctx, update.CompleteWithPruning()); err == nil {
+		return
+	}
+	slog.Debug("update completion with pruning failed; retrying", "error", err)
+	// 2. Second attempt with pruning
+	if err = u.UpdateRunner.Complete(ctx, update.CompleteWithPruning()); err == nil {
+		return
+	}
+	slog.Error("update completion with pruning failed; trying without pruning", "error", err)
+	// 3. Fallback to complete without pruning
+	if err = u.UpdateRunner.Complete(ctx); err == nil {
+		slog.Warn("completed update without pruning; some dangling blobs may remain")
+		return
+	}
+	// 4. Final attempt without pruning and force
+	if err = u.UpdateRunner.Complete(ctx, update.CompleteWithForce()); err == nil {
+		slog.Warn("completed update without pruning and with force; some dangling blobs may remain")
+		return
+	}
+	// 4. Total failure
+	slog.Error(
+		"failed to complete update after the app successfully started; some dangling blobs may remain",
+		"error", err,
+	)
 }
