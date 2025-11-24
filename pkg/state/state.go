0af01503f4be48416905178a0e55a3630b4de0eb
Mike Sul <mike.sul@foundries.io>
2025-11-24 13:16:44 +0100

    state: Add skeleton code for adding context to update events
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -5,6 +5,7 @@ package state
 
 import (
 	"context"
+	"encoding/json"
 	"log/slog"
 	"time"
 
@@ -75,11 +76,57 @@ var (
 
 func (u *UpdateContext) SendEvent(event events.EventTypeValue, eventErr ...error) {
 	var opts []events.EnqueueEventOption
+	var eventError error
 	if len(eventErr) > 0 {
 		eventStatus := eventErr[0] == nil
 		opts = append(opts, events.WithEventStatus(eventStatus))
+		eventError = eventErr[0]
+	}
+	if details := u.getUpdateDetails(event, eventError); details != "" {
+		opts = append(opts, events.WithEventDetails(details))
 	}
 	if err := u.EventSender.EnqueueEvent(event, u.UpdateRunner.Status().ID, u.ToTarget, opts...); err != nil {
 		slog.Error("failed to send event", "event", event, "err", err)
 	}
+}
+
+func (u *UpdateContext) getUpdateDetails(eventType events.EventTypeValue, eventErr error) string {
+	var detailsString string
+	var details interface{}
+	switch eventType {
+	case events.DownloadStarted:
+		details = u.getDownloadStartedDetails()
+	case events.DownloadCompleted:
+		details = u.getDownloadCompletedDetails(eventErr)
+	case events.InstallationStarted:
+		details = u.getInstallationStartedDetails()
+	case events.InstallationCompleted:
+		details = u.getInstallationCompletedDetails(eventErr)
+	}
+	if details == nil {
+		return ""
+	}
+	if detailBytes, err := json.MarshalIndent(details, "", "  "); err == nil {
+		detailsString = string(detailBytes)
+	} else {
+		slog.Error("failed to marshal event details; no any details will be added to the update event context",
+			"event", eventType, "err", err)
+	}
+	return detailsString
+}
+
+func (u *UpdateContext) getDownloadStartedDetails() interface{} {
+	return nil
+}
+
+func (u *UpdateContext) getDownloadCompletedDetails(eventErr error) interface{} {
+	return nil
+}
+
+func (u *UpdateContext) getInstallationStartedDetails() interface{} {
+	return nil
+}
+
+func (u *UpdateContext) getInstallationCompletedDetails(eventErr error) interface{} {
+	return nil
 }
