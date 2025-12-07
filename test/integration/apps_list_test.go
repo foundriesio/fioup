93fd64e4d22eeae327da51e90c27e1c6558e4bba
Andre Detsch <andre.detsch@foundries.io>
2025-12-07 20:38:11 -0300

    tests: Verify apps list changes
    Allow setting apps list, and verify that fioup correctly handles changes
    on that list.
    
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

---
+++ test/integration/apps_list_test.go
@@ -0,0 +1,36 @@
+package integration_tests
+
+import (
+	"testing"
+)
+
+// Verify that changes in apps list are correctly handled by the update logic
+func TestAppsList(t *testing.T) {
+	it := newIntegrationTest(t)
+
+	target1 := it.genNewTarget(100, 2, 50, false)
+
+	targets := []*Target{target1}
+	it.saveTargetsJson(targets)
+	it.testUpdateTo(target1, targets)
+
+	it.setApps([]string{"app-1"})
+	it.refreshConfig()
+	it.testUpdateTo(target1, targets)
+
+	it.setApps([]string{"app-2"})
+	it.refreshConfig()
+	it.testUpdateTo(target1, targets)
+
+	it.setApps(nil)
+	it.refreshConfig()
+	it.testUpdateTo(target1, targets)
+
+	it.setApps([]string{})
+	it.refreshConfig()
+	it.testUpdateTo(target1, targets)
+
+	it.setApps([]string{"app-1", "app-2"})
+	it.refreshConfig()
+	it.testUpdateTo(target1, targets)
+}
