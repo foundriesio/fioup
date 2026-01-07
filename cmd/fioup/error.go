deabf173b2b67ee20390e75220ba5132cd19f504
Andre Detsch <andre.detsch@foundries.io>
2026-01-07 11:17:02 -0300

    cmd: Return specific exit codes for known errors
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

--- cmd/fioup/error.go
+++ cmd/fioup/error.go
@@ -6,11 +6,45 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/foundriesio/fioup/pkg/state"
+	"github.com/pkg/errors"
 )
+
+var errorsMap = map[error]int{
+	state.ErrInvalidActionForState:   10,
+	state.ErrMetaUpdateFailed:        20,
+	state.ErrNoMatchingTarget:        21,
+	state.ErrTargetNotFound:          22,
+	state.ErrInvalidOngoingUpdate:    23,
+	state.ErrNoUpdateInProgress:      24,
+	state.ErrCheckNoUpdate:           25,
+	state.ErrNewerVersionIsAvailable: 26,
+	state.ErrInitFailed:              30,
+	state.ErrFetchFailed:             40,
+	state.ErrFetchNoSpace:            41,
+	state.ErrStopAppsFailed:          50,
+	state.ErrInstallFailed:           60,
+	state.ErrStartFailed:             70,
+}
+
+func errorToExitCode(err error) int {
+	var exitCode int
+	if err != nil {
+		exitCode = 1
+		for mappedErr, code := range errorsMap {
+			if errors.Is(err, mappedErr) {
+				exitCode = code
+				break
+			}
+		}
+	}
+	return exitCode
+}
 
 // DieNotNil logs the error and exits with code 1.
 func DieNotNil(err error, message ...string) {
-	DieNotNilWithCode(err, 1, message...)
+	DieNotNilWithCode(err, errorToExitCode(err), message...)
 }
 
 // DieNotNilWithCode logs the error and exits with the given code.
