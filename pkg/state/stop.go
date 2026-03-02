0ca8ad7bebf5180330b8cb4a4bcf05aafc28162c
Mike Sul <mike.sul@foundries.io>
2026-03-02 09:33:04 +0100

    refact(stop): stop app only if removed or need sync
    In daemon mode, if there is no new target update and only the app list
    has changed, stop only the apps that have been removed or are not
    healthy and require synchronization (not running/installed/fetched).
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- pkg/state/stop.go
+++ pkg/state/stop.go
@@ -12,6 +12,7 @@ import (
 	"github.com/foundriesio/composeapp/pkg/update"
 	"github.com/foundriesio/fioup/internal/events"
 	"github.com/foundriesio/fioup/pkg/status"
+	"github.com/foundriesio/fioup/pkg/target"
 )
 
 type Stop struct{}
@@ -31,10 +32,39 @@ func (s *Stop) Execute(ctx context.Context, updateCtx *UpdateContext) error {
 		return fmt.Errorf("%w: invalid state %s for stopping apps; must be in state %s or %s", ErrInvalidActionForState,
 			currentState, update.StateFetched, update.StateInstalling)
 	}
-	// Installation starts from stopping the currently running apps that are being updated
+	// Installation starts from stopping of the required apps
 	updateCtx.SendEvent(events.InstallationStarted)
-	// Stop apps being updated before installing their updates
-	err := compose.StopApps(ctx, updateCtx.Config.ComposeConfig(), updateCtx.FromTarget.AppURIs())
+
+	var appsToStop target.Apps
+	if updateCtx.Type == UpdateTypeUpdate || updateCtx.IsForcedUpdate {
+		// Stop all apps if it is a target/version update or a forced update.
+		appsToStop = updateCtx.FromTarget.Apps
+	} else {
+		// If it is a sync non-forced update, only stop the apps that are being removed or
+		// those in sync list that are not "healthy"(running/installed/fetched).
+		appsToStop = append(appsToStop, updateCtx.AppDiff.Remove...)
+		for _, app := range updateCtx.AppDiff.Sync {
+			if updateCtx.CurrentStatus == nil {
+				// Handle edge case if the Stop state is executed without running the "Check" state that populates the CurrentStatus
+				slog.Warn("current apps' status is not available in update context, " +
+					"will stop app in sync list without checking their status")
+				appsToStop = append(appsToStop, app)
+				continue
+			}
+			appStatus, ok := updateCtx.CurrentStatus.AppStatuses[app.URI]
+			if !ok {
+				appsToStop = append(appsToStop, app)
+				slog.Warn("app in sync list not found in current status", "appURI", app.URI)
+				continue
+			}
+			if !appStatus.Running || !appStatus.Installed || !appStatus.Fetched {
+				appsToStop = append(appsToStop, app)
+				continue
+			}
+		}
+	}
+	slog.Debug("apps to stop", "updateType", updateCtx.Type, "isForcedUpdate", updateCtx.IsForcedUpdate, "apps", appsToStop.Names())
+	err := compose.StopApps(ctx, updateCtx.Config.ComposeConfig(), appsToStop.URIs())
 	if err != nil {
 		// If stopping apps failed, it means that update has completed with failure, so send InstallationCompleted event with failure
 		if currentStatus, errStatus := status.GetCurrentStatus(ctx, updateCtx.Config.ComposeConfig()); errStatus == nil {
